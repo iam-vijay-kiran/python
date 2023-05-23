62be2c04e3eae36dc323513f22e5fb9f80be9e5c
iam-vijay-kiran <105585747+iam-vijay-kiran@users.noreply.github.com>
2023-05-23 23:33:48 +0530

    Update 14.Python_Class_and_Objects.py

--- 14.Python_Class_and_Objects.py
+++ 14.Python_Class_and_Objects.py
@@ -1,1 +1,17 @@
+###  OOPS through Python  ###
+## Class --> objects (functions)
+# class has attributes & behaviour 
+# attribute = stores values for object   &&   behaviour = methods which shows the properties of a particular object
 
+# How to create a class in python
+
+class Employee:
+
+    # Constructor of class
+    def __init__(self, name, salary):
+        self.emp_name = name
+        self.emp_salary = salary 
+    
+    # method of class
+    def displayEmployeeInfo(self):
+        print("Employee name : ", self.emp_name, "Employee Salary : ", self.emp_salary) 
