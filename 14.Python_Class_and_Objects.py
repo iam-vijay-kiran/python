00a6749bffc0e6e5b6b6dd9f59e7cd35f1f8455a
iam-vijay-kiran <105585747+iam-vijay-kiran@users.noreply.github.com>
2023-05-24 13:32:20 +0530

    Update 14.Python_Class_and_Objects.py

--- 14.Python_Class_and_Objects.py
+++ 14.Python_Class_and_Objects.py
@@ -6,12 +6,24 @@
 # How to create a class in python
 
 class Employee:
-
     # Constructor of class
+    # it is mainly used for assignment of instance variables
     def __init__(self, name, salary):
+        # instance variable or instance attributes 
         self.emp_name = name
         self.emp_salary = salary 
-    
+
     # method of class
     def displayEmployeeInfo(self):
         print("Employee name : ", self.emp_name, "Employee Salary : ", self.emp_salary) 
+
+emp1 = Employee("vijay", 4500)
+emp2 = Employee("ganesh", 4700)
+emp1.displayEmployeeInfo()
+emp2.displayEmployeeInfo()
+        
+print(emp1.emp_name)          # let emp1 @ 200 memory location, then emp1.emp_name calls name @ emp1 location
+print(emp2.emp_name)          # same with the emp2
+                              # self = reference for obj we are creating  
+
+#Difference between Class Variable and Instance Variable
